2e0412632ea53f1ec132a3b9f8f8b2390cb77661
Aurelien heude <aurelienheudepro@gmail.com>
2021-10-20 10:06:12 +0200

    Menu presque fini

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
-function menu()
+function menuOpen()
 {
+    let menu = document.getElementById('nav');
+    let wrapper = document.getElementById('wrapper');
 
+    menuWidth = menu.offsetWidth;
+
+    if(menuWidth == 1)
+    {
+        menu.style.opacity = 1;
+        menu.style.width = "267px";
+
+        wrapper.style.display = "flex";
+    }
+}
+
+function menuClose()
+{
+    let menu = document.getElementById('nav');
+
+    menuWidth = menu.offsetWidth;
+
+    if(menuWidth == 267)
+    {
+        menu.style.opacity = 0;
+        menu.style.width = "1px";
+
+        wrapper.style.display = "none";
+    }
 }
