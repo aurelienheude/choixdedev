defd87c126e635e2980b0ea55e1d753090776bd0
Aurelien heude <aurelienheudepro@gmail.com>
2021-10-21 10:08:23 +0200

    reglage du probleme de transition et aide de blazt

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-function menuOpen()
-{
-    let menu = document.getElementById('nav');
-    let wrapper = document.getElementById('wrapper');
-
-    menuWidth = menu.offsetWidth;
-
-    if(menuWidth == 1)
-    {
-        menu.style.opacity = 1;
-        menu.style.width = "320px";
-
-        wrapper.style.display = "flex";
-    }
+/*******************************   BOUTON MENU   *******************************/
+const menuOpen = (menu) => {
+    menu.classList.add("transition_menu");
+    menu.classList.add("open");
 }
 
-function menuClose()
-{
-    let menu = document.getElementById('nav');
-
-    menuWidth = menu.offsetWidth;
-
-    if(menuWidth == 320)
-    {
-        menu.style.opacity = 0;
-        menu.style.width = "1px";
-
-        wrapper.style.display = "none";
-    }
+const menuClose = (menu) => {
+    menu.classList.remove("open");
 }
+
+/*****   SECTION CHOICE   *****/
+
+
+/*****   Ecouteur d'évennement   *****/
+window.addEventListener("load", () => {
+    const menu = document.getElementById("nav");
+
+    const buttonOpen = document.getElementById("buttonO");
+    const buttonClose = document.getElementById("buttonC");
+
+    buttonOpen.addEventListener("click", (e) => menuOpen(menu));
+    buttonClose.addEventListener("click", (e) => menuClose(menu));
+
+//////////////////////////////////////////////////////////////////////////////
+
+    const choiceOnebloc = document.getElementById("choiceOne");
+    const choiceTwobloc = document.getElementById("choiceTwo");   
+
+    choiceOnebloc.addEventListener("mouseover", () =>{
+        choiceOnebloc.style.opacity = "1";
+        choiceTwobloc.style.opacity = "0.7";
+    });
+
+    choiceTwobloc.addEventListener("mouseover", () =>{
+        choiceTwobloc.style.opacity = "1";
+        choiceOnebloc.style.opacity = "0.7";
+    });
+});
